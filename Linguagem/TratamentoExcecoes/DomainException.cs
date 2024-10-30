ee4095fdc791c34de67e8f16535ae9752ad2519c
Paulo Carvalho de Medeiros Filho <paulofilho055@gmail.com>
2024-10-29 22:12:41 -0300

    Tratamento de exceções

---
+++ Linguagem/TratamentoExcecoes/DomainException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TratamentoExcecoes
+{
+    //Utilizando um exceção personalizada
+    internal class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message){ }
+    }
+}
---
+++ Linguagem/TratamentoExcecoes/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Net.Http.Headers;
+
+namespace TratamentoExcecoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TryCatchFinally();
+
+            Console.ReadKey();
+
+        }
+
+        static void TryCatchFinally()
+        {
+
+            try
+            {
+                //Existem as SystemException e as ApplicationException que herdam de Exception
+                //Uma exceção personalizada, DomainException herda de ApplicationExcepion
+                ExceptionMethod();
+            }
+            catch (DomainException ex)
+            {
+                //Caso seja lançada exceção no bloco try, o bloco catch é executado
+                Console.WriteLine($"Message: {ex.Message}");
+                Console.WriteLine($"StackTrace: {ex.StackTrace}");
+                Console.WriteLine($"InnerException: { ex?.InnerException?.Message}");
+            }
+            finally
+            {
+                //O bloco finally é sempre executado, caso exista exeção no bloco try ou não.
+                //Adequadao para liberar recursos
+                Console.WriteLine("Bloco finally executado");
+            }
+        }
+        
+        static void ExceptionMethod()
+        {
+            throw new DomainException("Erro de domínio.");
+        }
+    }
+}
